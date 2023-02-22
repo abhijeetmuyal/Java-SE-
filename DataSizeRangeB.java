b46e42fb3ec318e6772497ad7bfe0f5190d0f747
abhijeetmuyal <126005820+abhijeetmuyal@users.noreply.github.com>
2023-02-22 18:20:19 +0530

    Add files via upload

---
+++ DataSizeRangeB.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class DataSizeRangeB
+{
+	public static void main(String args[])
+	{
+		System.out.println("Byte Min: "+Byte.MIN_VALUE);
+		System.out.println("Byte Max: "+Byte.MAX_VALUE);
+		System.out.println("Byte Bytes: "+Byte.BYTES);
+	}
+}
